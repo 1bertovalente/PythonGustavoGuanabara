cc16e3eb83b7d7c6d460b6baf2ff1e761b1cbb09
Humberto Valente <paulohnvalente@gmail.com>
2019-11-14 10:47:55 -0300

    Add ex011

---
+++ ex011.py
@@ -0,0 +1,7 @@
+largura = float(input('Informe a largura:'))
+altura = float(input('Informe a altura:'))
+
+area = largura * altura
+pintar = area / 2 
+
+print('Area de {}m² e para pintar essa parede, precisamos de {} l de tinta'.format(area, pintar))

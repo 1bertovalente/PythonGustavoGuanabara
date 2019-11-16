3018ddbe21641be1fd26e8b9e8c15998a65e4fdd
Humberto Valente <paulohnvalente@gmail.com>
2019-11-16 16:16:30 -0300

    Tipo primitivo

--- ex004.py
+++ ex004.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
 algo = input('Digite algo:')
 
 print('O tipo primitivo é:', type(algo), '\nSó tem espaços?', algo.isspace(), '\nÉ um número?',
